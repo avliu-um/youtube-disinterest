cd195831c742e352537641cfb78be7a629485632
Alexander Liu <avliu@m-fvfg61a0q6lw.local>
2022-05-27 10:59:37 -0400

    refactoring the runner file

--- runner.py
+++ runner.py
@@ -2,17 +2,18 @@ import pandas as pd
 from scrub_main import scrub_experiment
 
 
-def get_attributes():
-    my_row = 0
-    runs_filepath = 'runs/alt-right_runs.csv'
+def get_attributes(runs_filepath, row):
     runs = pd.read_csv(runs_filepath).to_dict('index')
-    attributes = runs[my_row]
+    attributes = runs[row]
 
     return attributes
 
 
 def main():
-    attributes = get_attributes()
+    runs_filepath = 'runs/alt-right_runs.csv'
+    my_row = 0
+
+    attributes = get_attributes(runs_filepath, my_row)
     scrub_experiment(attributes)
 
 
